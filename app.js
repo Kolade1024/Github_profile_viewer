06420fec27142998558e41b1e6a9f1d3515a4606
Kolade1024 <akoladeemmanuel14@gmail.com>
2023-11-02 09:14:49 +0100

    Update

--- app.js
+++ app.js
@@ -5,20 +5,26 @@ const inputField = SEARCH_BAR.querySelector("#search");
 //SEARCH RESULT
 const AVATAR = document.querySelector(".search_result .result .avatar img");
 
-console.log(AVATAR);
+
 
 
 
 SEARCH_BTN.addEventListener("click", (e)=>{
   e.preventDefault();
   let username = inputField.value;
-  let API_URL = `https://api.github.com/users/${username}`;
+  let API_URL = `https://api.github.com/search/users?q=${username}`;
+
   
   let fetchUsers = async ()=>{
     let response  = await fetch(API_URL);
     let data = await response.json();
-    console.log(data["avatar_url"]);
-    AVATAR.src = data["avatar_url"];
-  }
-  fetchUsers();
-})
+    
+      data.forEach(profile => {
+        
+        
+      });
+}
+  
+     fetchUsers();
+}
+)
